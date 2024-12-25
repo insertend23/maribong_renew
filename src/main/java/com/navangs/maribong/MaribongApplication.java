c51b1917441a911cb51669feb52de35457ac94f3
navangs <43688529+insertend23@users.noreply.github.com>
2024-12-26 00:39:53 +0900

    init commit

---
+++ src/main/java/com/navangs/maribong/MaribongApplication.java
@@ -0,0 +1,13 @@
+package com.navangs.maribong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaribongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaribongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navangs/maribong/MaribongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navangs.maribong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaribongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
