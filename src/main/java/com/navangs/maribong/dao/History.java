2299ed761fae0a7a9269f7f9855e45dbc67fe1c9
navangs <43688529+insertend23@users.noreply.github.com>
2025-01-10 21:26:00 +0900

    fix: add column settings

--- src/main/java/com/navangs/maribong/dao/History.java
+++ src/main/java/com/navangs/maribong/dao/History.java
@@ -1,16 +1,34 @@
 package com.navangs.maribong.dao;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 public class History {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     String id;
-    String userId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id")
+    User user;
+
+    @Column(nullable = false)
     String title;
-    String term;
+
+    @Column(nullable = false)
+    LocalDateTime startDate;
+
+    @Column(nullable = false)
+    LocalDateTime endDate;
 }
--- src/main/java/com/navangs/maribong/dao/User.java
+++ src/main/java/com/navangs/maribong/dao/User.java
@@ -1,29 +1,49 @@
 package com.navangs.maribong.dao;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
+@Table(name = "user_info")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
+
+    @Column(nullable = false)
     private String pwd;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    private String sex;
+
+    @Column(nullable = false)
+    private String birthYear;
+
+    @Column(nullable = false)
+    private String birthMonth;
+
+    @Column(nullable = false)
+    private String pushChk;
+
+    @ColumnDefault("current_timestamp()")
+    @Column(nullable = false)
+    private LocalDateTime regDate;
+
+    @UpdateTimestamp
+    @ColumnDefault("current_timestamp()")
+    @Column(nullable = false)
+    private LocalDateTime modDate;
+
     private String token;
     private String profile;
-    private String sex;
-    private String birthYear;
-    private String birthMonth;
-    private String homeCategory;
-    private String city1;
-    private String city2;
-    private String pushChk;
-    private LocalDateTime regDate;
-    @UpdateTimestamp
-    private LocalDateTime modDate;
 }
