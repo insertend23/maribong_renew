c0e4dd8c2709c1e1d48097dde7f148cee1739b4e
navangs <43688529+insertend23@users.noreply.github.com>
2025-01-15 03:55:01 +0900

    fix: correct id type

--- src/main/java/com/navangs/maribong/repository/HistoryRepository.java
+++ src/main/java/com/navangs/maribong/repository/HistoryRepository.java
@@ -3,6 +3,6 @@ package com.navangs.maribong.repository;
 import com.navangs.maribong.dao.History;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HistoryRepository extends JpaRepository<History, Long> {
+public interface HistoryRepository extends JpaRepository<History, Integer> {
     History findByUserId(String userId);
 }
--- src/main/java/com/navangs/maribong/repository/QuizRepository.java
+++ src/main/java/com/navangs/maribong/repository/QuizRepository.java
@@ -3,6 +3,6 @@ package com.navangs.maribong.repository;
 import com.navangs.maribong.dao.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    Quiz findQuizById(Long id);
+public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+    Quiz findQuizById(Integer id);
 }
