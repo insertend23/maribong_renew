fd8675961dd34ea7252cc0f724de8e4f349542c0
navangs <43688529+insertend23@users.noreply.github.com>
2025-01-15 03:54:25 +0900

    fix: set column length

--- src/main/java/com/navangs/maribong/dao/History.java
+++ src/main/java/com/navangs/maribong/dao/History.java
@@ -29,7 +29,7 @@ public class History {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String title;
 
     @Column(nullable = false)
