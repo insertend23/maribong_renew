5079cb48b592b7b9d27fad0315617bb91e97664f
navangs <43688529+insertend23@users.noreply.github.com>
2025-01-15 03:54:06 +0900

    fix: correct types and rename date to timestamp

--- src/main/java/com/navangs/maribong/dao/User.java
+++ src/main/java/com/navangs/maribong/dao/User.java
@@ -7,43 +7,49 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name = "user_info")
+@DynamicInsert
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
     @Id
+    @Column(length = 20)
     private String id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 20)
     private String pwd;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String name;
 
     @Column(nullable = false)
-    private String sex;
+    private Character gender;
 
     @Column(nullable = false)
-    private String birthYear;
+    private Integer birthYear;
 
     @Column(nullable = false)
-    private String birthMonth;
+    private Integer birthMonth;
 
+    @Column(columnDefinition = "TINYINT(1)")
     @ColumnDefault("1")
-    private String pushChk;
+    private Boolean pushChk;
 
     @ColumnDefault("current_timestamp()")
-    private LocalDateTime regDate;
+    private LocalDateTime regTimestamp;
 
     @UpdateTimestamp
     @ColumnDefault("current_timestamp()")
-    private LocalDateTime modDate;
+    private LocalDateTime modTimestamp;
 
     private String token;
     private String profile;
