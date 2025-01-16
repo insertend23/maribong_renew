5997b7952dfe81ebaba315d445f40eb23129a020
navangs <43688529+insertend23@users.noreply.github.com>
2025-01-16 23:16:46 +0900

    fix: test each case with rollback

--- src/test/java/com/navangs/maribong/DataJpaCustomTest.java
+++ src/test/java/com/navangs/maribong/DataJpaCustomTest.java
@@ -7,14 +7,11 @@ import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource("classpath:application-test.properties")
-@Transactional(propagation = Propagation.NOT_SUPPORTED)
 public @interface DataJpaCustomTest {
 }
