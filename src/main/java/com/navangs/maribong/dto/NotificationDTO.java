7688dfba188961a6b847875590e8edeb394a4163
navangs <43688529+insertend23@users.noreply.github.com>
2025-01-22 03:25:49 +0900

    feat: add Notification DTO conversion method

--- src/main/java/com/navangs/maribong/dto/NotificationDTO.java
+++ src/main/java/com/navangs/maribong/dto/NotificationDTO.java
@@ -1,8 +1,25 @@
 package com.navangs.maribong.dto;
 
+import com.navangs.maribong.domain.Notification;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class NotificationDTO {
-    String category;
+    Integer category;
     String userName;
     String message;
+    String userId;
     Long postId;
+
+    public static NotificationDTO fromEntity(Notification notification) {
+        return NotificationDTO.builder()
+            .category(notification.getCategory())
+            .userName(notification.getUserName())
+            .message(notification.getId().getMessage())
+            .userId(notification.getId().getUserId())
+            .postId(notification.getId().getPostId())
+            .build();
+    }
 }
