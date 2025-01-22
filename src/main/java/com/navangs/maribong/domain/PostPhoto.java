15766c83fb08a71defb658083d269be3e4950b3e
navangs <43688529+insertend23@users.noreply.github.com>
2025-01-22 20:05:51 +0900

    fix: set fetch type and optional

--- src/main/java/com/navangs/maribong/domain/PostPhoto.java
+++ src/main/java/com/navangs/maribong/domain/PostPhoto.java
@@ -2,6 +2,7 @@ package com.navangs.maribong.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +27,7 @@ public class PostPhoto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "post_id")
     private Post post;
