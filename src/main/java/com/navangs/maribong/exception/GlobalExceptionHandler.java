b4dc82b03a2a5f8763570279ca1cc9a05538a467
navangs <43688529+insertend23@users.noreply.github.com>
2025-01-23 15:19:48 +0900

    fix: swagger conflict with controller advice

--- src/main/java/com/navangs/maribong/exception/GlobalExceptionHandler.java
+++ src/main/java/com/navangs/maribong/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.navangs.maribong.exception;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MaribongException.class)
