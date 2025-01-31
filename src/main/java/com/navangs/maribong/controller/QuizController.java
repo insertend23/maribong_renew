64013b5ec40e1a13cc4dbd9a0f0a1f0b4ff6fa73
navangs <43688529+insertend23@users.noreply.github.com>
2025-02-01 03:48:17 +0900

    feat: complete getting quiz api

--- src/main/java/com/navangs/maribong/controller/QuizController.java
+++ src/main/java/com/navangs/maribong/controller/QuizController.java
@@ -1,7 +1,10 @@
 package com.navangs.maribong.controller;
 
+import com.navangs.maribong.dto.QuestionDTO;
 import com.navangs.maribong.dto.QuizAnswerDTO;
 import com.navangs.maribong.response.QuestionResponse;
+import com.navangs.maribong.service.QuizService;
+import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping(value = "/quiz")
 public class QuizController {
+    private static final String NO_ASSIGNED_QUIZ_MSG = "아직 등록된 퀴즈가 없습니다.";
+    private final QuizService quizService;
+
     @RequestMapping(value = "getQuiz", method = {RequestMethod.GET, RequestMethod.POST})
     public QuestionResponse getQuiz(String userId) {
-        return null;
+        Long quizId = quizService.getAssignedQuizId(userId);
+
+        if (quizId != null) {
+            List<QuestionDTO> questions = quizService.getQuestions(quizId);
+
+            return QuestionResponse.getSuccessResponse(questions);
+        }
+
+        return QuestionResponse.getFailureResponse(NO_ASSIGNED_QUIZ_MSG);
     }
 
     @PostMapping(value = "sendQuiz")
--- src/main/java/com/navangs/maribong/response/QuestionResponse.java
+++ src/main/java/com/navangs/maribong/response/QuestionResponse.java
@@ -1,6 +1,7 @@
 package com.navangs.maribong.response;
 
 import com.navangs.maribong.dto.QuestionDTO;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
@@ -8,7 +9,23 @@ import lombok.Data;
 @Data
 @Builder
 public class QuestionResponse {
-    String result;
-    String resultMsg;
-    List<QuestionDTO> list;
+    private String result;
+    private String resultMsg;
+    private List<QuestionDTO> list;
+
+    public static QuestionResponse getSuccessResponse(List<QuestionDTO> questions) {
+        return QuestionResponse.builder()
+            .result("y")
+            .resultMsg("")
+            .list(questions)
+            .build();
+    }
+
+    public static QuestionResponse getFailureResponse(String resultMessage) {
+        return QuestionResponse.builder()
+            .result("n")
+            .resultMsg(resultMessage)
+            .list(new ArrayList<>())
+            .build();
+    }
 }
