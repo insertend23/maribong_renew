f941b6bcb3bd4d14958f79a88a0a5da428aacbe3
navangs <43688529+insertend23@users.noreply.github.com>
2025-02-01 15:59:10 +0900

    fix: response returns super class data

--- src/main/java/com/navangs/maribong/response/QuestionResponse.java
+++ src/main/java/com/navangs/maribong/response/QuestionResponse.java
@@ -3,9 +3,13 @@ package com.navangs.maribong.response;
 import com.navangs.maribong.dto.QuestionDTO;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
 
+@Data
+@EqualsAndHashCode(callSuper = false)
 @SuperBuilder
 public class QuestionResponse extends BaseResponse {
     private String resultMsg;
