d20f33fec7931ee332589e96ef6062334e7a8b0d
navangs <43688529+insertend23@users.noreply.github.com>
2025-02-04 21:19:49 +0900

    fix: image save dir not correct

--- src/main/java/com/navangs/maribong/service/impl/PostImageServiceImpl.java
+++ src/main/java/com/navangs/maribong/service/impl/PostImageServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class PostImageServiceImpl implements ImageService {
-    public static final String POST_IMG_UPLOAD_PATH = "./src/main/uploads/postimg";
+    public static final String POST_IMG_UPLOAD_PATH = "./src/main/uploads/postimg/";
 
     @Override
     public void uploadImage(MultipartFile image, String imageName) {
